d9686a3f4720b8718321e732ad7555cae06ef859
i-am-it <amyvats@gmail.com>
2018-08-09 01:50:42 +0530

    1stcommit

---
+++ index.js
@@ -0,0 +1,288 @@
+$('#icoh').collapse('show')
+$('#brand').collapse('show')
+
+$('#megha').on('show.bs.collapse', function () {
+  console.log("hua");
+  $('#brand').collapse('hide')
+  $('#icoh').collapse('hide')
+  $('#ruchita').collapse('hide')
+  $('#maa').collapse('hide')
+  $('#travel').collapse('hide')
+  $('#family').collapse('hide')
+  $('#innova').collapse('hide')
+  $('#trump').collapse('hide')
+  $('#ee').collapse('hide')
+  $('#megh').collapse('show')
+  $('#ruchh').collapse('hide')
+  $('#maah').collapse('hide')
+  $('#trah').collapse('hide')
+  $('#famh').collapse('hide')
+  $('#innoh').collapse('hide')
+  $('#eeh').collapse('hide')
+  $('#trumh').collapse('hide')
+})
+
+$('#ruchita').on('show.bs.collapse', function () {
+  console.log("hua");
+  $('#brand').collapse('hide')
+  $('#icoh').collapse('hide')
+  $('#megha').collapse('hide')
+  $('#maa').collapse('hide')
+  $('#travel').collapse('hide')
+  $('#family').collapse('hide')
+  $('#innova').collapse('hide')
+  $('#trump').collapse('hide')
+  $('#ee').collapse('hide')
+  $('#megh').collapse('hide')
+  $('#ruchh').collapse('show')
+  $('#maah').collapse('hide')
+  $('#trah').collapse('hide')
+  $('#famh').collapse('hide')
+  $('#innoh').collapse('hide')
+  $('#eeh').collapse('hide')
+  $('#trumh').collapse('hide')
+})
+
+$('#maa').on('show.bs.collapse', function () {
+  console.log("hua");
+  $('#brand').collapse('hide')
+  $('#icoh').collapse('hide')
+  $('#megha').collapse('hide')
+  $('#ruchita').collapse('hide')
+  $('#travel').collapse('hide')
+  $('#family').collapse('hide')
+  $('#innova').collapse('hide')
+  $('#trump').collapse('hide')
+  $('#ee').collapse('hide')
+  $('#megh').collapse('hide')
+  $('#ruchh').collapse('hide')
+  $('#maah').collapse('show')
+  $('#trah').collapse('hide')
+  $('#famh').collapse('hide')
+  $('#innoh').collapse('hide')
+  $('#eeh').collapse('hide')
+  $('#trumh').collapse('hide')
+})
+
+$('#travel').on('show.bs.collapse', function () {
+  console.log("hua");
+  $('#brand').collapse('hide')
+  $('#icoh').collapse('hide')
+  $('#megha').collapse('hide')
+  $('#maa').collapse('hide')
+  $('#ruchita').collapse('hide')
+  $('#family').collapse('hide')
+  $('#innova').collapse('hide')
+  $('#trump').collapse('hide')
+  $('#ee').collapse('hide')
+  $('#megh').collapse('hide')
+  $('#ruchh').collapse('hide')
+  $('#maah').collapse('hide')
+  $('#trah').collapse('show')
+  $('#famh').collapse('hide')
+  $('#innoh').collapse('hide')
+  $('#eeh').collapse('hide')
+  $('#trumh').collapse('hide')
+})
+
+$('#family').on('show.bs.collapse', function () {
+  console.log("hua");
+  $('#brand').collapse('hide')
+  $('#icoh').collapse('hide')
+  $('#megha').collapse('hide')
+  $('#maa').collapse('hide')
+  $('#travel').collapse('hide')
+  $('#ruchita').collapse('hide')
+  $('#innova').collapse('hide')
+  $('#trump').collapse('hide')
+  $('#ee').collapse('hide')
+  $('#megh').collapse('hide')
+  $('#ruchh').collapse('hide')
+  $('#maah').collapse('hide')
+  $('#trah').collapse('hide')
+  $('#famh').collapse('show')
+  $('#innoh').collapse('hide')
+  $('#eeh').collapse('hide')
+  $('#trumh').collapse('hide')	
+})
+
+$('#innova').on('show.bs.collapse', function () {
+  console.log("hua");
+  $('#brand').collapse('hide')
+  $('#icoh').collapse('hide')
+  $('#megha').collapse('hide')
+  $('#maa').collapse('hide')
+  $('#travel').collapse('hide')
+  $('#family').collapse('hide')
+  $('#ruchita').collapse('hide')
+  $('#trump').collapse('hide')
+  $('#ee').collapse('hide')
+  $('#megh').collapse('hide')
+  $('#ruchh').collapse('hide')
+  $('#maah').collapse('hide')
+  $('#trah').collapse('hide')
+  $('#famh').collapse('hide')
+  $('#innoh').collapse('show')
+  $('#eeh').collapse('hide')
+  $('#trumh').collapse('hide')
+})
+
+$('#trump').on('show.bs.collapse', function () {
+  console.log("hua");
+  $('#brand').collapse('hide')
+  $('#icoh').collapse('hide')
+  $('#megha').collapse('hide')
+  $('#maa').collapse('hide')
+  $('#travel').collapse('hide')
+  $('#family').collapse('hide')
+  $('#innova').collapse('hide')
+  $('#ruchita').collapse('hide')
+  $('#ee').collapse('hide')
+  $('#megh').collapse('hide')
+  $('#ruchh').collapse('hide')
+  $('#maah').collapse('hide')
+  $('#trah').collapse('hide')
+  $('#famh').collapse('hide')
+  $('#innoh').collapse('hide')
+  $('#eeh').collapse('hide')
+  $('#trumh').collapse('show')
+  $('#trumh').collapse('hide')
+})
+
+$('#ee').on('show.bs.collapse', function () {
+  console.log("hua");
+  $('#brand').collapse('hide')
+  $('#icoh').collapse('hide')
+  $('#megha').collapse('hide')
+  $('#maa').collapse('hide')
+  $('#travel').collapse('hide')
+  $('#family').collapse('hide')
+  $('#innova').collapse('hide')
+  $('#trump').collapse('hide')
+  $('#ruchita').collapse('hide')
+  $('#megh').collapse('hide')
+  $('#ruchh').collapse('hide')
+  $('#maah').collapse('hide')
+  $('#trah').collapse('hide')
+  $('#famh').collapse('hide')
+  $('#innoh').collapse('hide')
+  $('#eeh').collapse('show')
+  $('#trumh').collapse('hide')	
+})
+
+
+
+
+document.getElementById('megc').onclick = function checkPosition() {
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        //...
+
+        	console.log("nhi hua");
+        	document.getElementById('chatwindow').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatbox').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+        	//document.getElementById('chatbox').style.height = "0px";
+        	//document.getElementById('chatwindow').style.height = "89vh"
+        	document.getElementById('chatwindowh').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatboxh').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+
+        	// document.getElementById('chatbox').style.height = "0px";
+        	// document.getElementById('chatwindow').style.height = "89vh"
+        	
+         
+    } 
+    document.getElementById('badgem').style.backgroundColor = "white";
+}
+
+document.getElementById('ruchc').onclick = function checkPosition() {
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        //...
+
+        	console.log("nhi hua");
+        	document.getElementById('chatwindow').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatbox').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+        	document.getElementById('chatwindowh').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatboxh').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+         
+    } 
+}
+
+document.getElementById('maac').onclick = function checkPosition() {
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        //...
+
+        	document.getElementById('chatwindow').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatbox').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+        	document.getElementById('chatwindowh').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatboxh').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+         
+    } 
+}
+
+document.getElementById('trac').onclick = function checkPosition() {
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        //...
+
+        	document.getElementById('chatwindow').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatbox').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+        	document.getElementById('chatwindowh').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatboxh').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+         
+    } 
+}
+
+document.getElementById('famc').onclick = function checkPosition() {
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        //...
+
+        	document.getElementById('chatwindow').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatbox').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+        	document.getElementById('chatwindowh').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatboxh').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+         	
+    }
+    document.getElementById('badgef').style.backgroundColor = "white"; 
+}
+
+document.getElementById('innoc').onclick = function checkPosition() {
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        //...
+
+        	document.getElementById('chatwindow').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatbox').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+        	document.getElementById('chatwindowh').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatboxh').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+         
+    } 
+}
+
+document.getElementById('eec').onclick = function checkPosition() {
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        //...
+
+        	document.getElementById('chatwindow').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatbox').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+        	document.getElementById('chatwindowh').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatboxh').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+         
+    } 
+}
+
+document.getElementById('trumc').onclick = function checkPosition() {
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        //...
+
+        	document.getElementById('chatwindow').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatbox').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+        	document.getElementById('chatwindowh').setAttribute( "class", "col-12 col-lg-8" );
+        	document.getElementById('chatboxh').setAttribute( "class", "col-0 col-lg-4 chat-window" );
+         
+    } 
+    document.getElementById('badget').style.backgroundColor = "white";
+}
+
+$('.custom-file-input').on('change',function(){
+  var fileName = $(this).val();
+  $(this).next('.custom-file-label').addClass("selected").html(fileName);
+})
+
+
